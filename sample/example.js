00140ffd49d1d98e0c0e37ede59080d3f2604d7c
VansonLeung <vanson-hk@hotmail.com>
2018-04-09 20:26:34 +0800

    Update example.js

--- sample/example.js
+++ sample/example.js
@@ -6,6 +6,7 @@ import {
     Image,
 } from 'react-native';
 
+// Import different kinds of transitions here / create & roll out your own transitions
 import {
   TransitionView,
   FadeTransitionChildView,
@@ -31,6 +32,7 @@ export default class extends React.Component {
   }
 
   onChange() {
+    // in this example here, it randomly picks a transition type from the array below when changing every time
     const SpecificTransitionChildViews = [
 	  FadeTransitionChildView,
       ShowcaseTransitionChildView,
@@ -46,7 +48,12 @@ export default class extends React.Component {
           <Image style={{flex: 1, alignSelf: 'stretch', resizeMode: 'stretch'}}
               source={{uri: `https://picsum.photos/2000/2000&t=${new Date().getTime()}`}}
               onLoad={() => {
+
+		// This is where the magic of seamless transition relies...
+		// When a network image has been loaded into memory completely, consume / start the transition here
                 this.transitionView.consume();
+
+		// Recursively schedule another "onChange()" after 3 secs
                 setTimeout(() => {
                   this.onChange();
                 }, 3000);
@@ -54,6 +61,7 @@ export default class extends React.Component {
           />
         </View>
     ), {
+      // Specify transition here
       SpecificTransitionChildView: SpecificTransitionChildView,
     })
   }
